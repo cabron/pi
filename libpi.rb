1c53d47e697579d6fcb9923fb25c60a6508a50e0
Piotr Czarnecki <paxsa@vp.pl>
2011-05-29 15:24:29 +0200

    Lexer, tokenize, parse, eval

--- libpi.rb
+++ libpi.rb
@@ -38,142 +38,163 @@ module Pi
 		COMMENT = 6
 		
 		
-		def initialize s=nil
+		def initialize
 			@arg = []
 			@c = {}
-			return if not s.is_a?(StringScanner)
-			
-			@pos = s.pos
-			if s.check(/#!(.*)\n/) && s.pos == 0
-				@t = COMMENT
-				@str = s.scan(/#!(.*)\n/)
-			elsif r = s.scan(/##\{([\s\S]*)\}|##(.*)\n/)
-				@t = COMMENT
-				@str = r
-			elsif r = s.scan(/-?[0-9]+([0-9a-zA-Z_$xXoOhHrRbB]*)/)
-				@t = NUMBER
-				v = ''
-				base = 10
-				r.each_char {|c|
-					if base == 10
-						if 'hHxX$'[c]
-							base = 16
-						elsif 'oO'[c]
-							base = 8
-						elsif 'rR'[c]
-							base = v.to_i
-							v = ''
-						else
-							v += c
-						end
-					else
-						v += c
-					end
-				}
-				
-				if base == 10 and (v['b'] or v['B'])
-					r = v
-					v = ''
-					r.each_char {|c|
-						if '01'[c]
-							v += c
-						elsif 'bB'[c]
-							base = 2
-						end
-					}
-				end
-				
-				#puts "NUMBER #{v}(#{base})"
-				@v = v.to_i(base)
-				
-			elsif r = s.scan(/[a-zA-Z$_][a-zA-Z0-9$_]*/)
-				@t = LABEL
-				@v = r
-			elsif r = s.scan(/[\(\)\[\]\{\}]/)
-				@str = r
-				@t = BRACKET
-				@v = case r
-					when '['..']' then	SQUARE
-					when '('..')' then	ROUND
-					when '{'..'}' then	CURLY
-				end
-				if ['[', '(', '{', '<'].include? r
-					@v |= LEFT
-				end
-			elsif r = s.scan(/\+\+|--|@|->|!/)
-				@str = r
-				@t = UNARY
-				@v = case r
-					when '++' then INC
-					when '--' then DEC
-					when '@' then AT
-					when '->' then POINTER
-				end
-			elsif r = s.scan(/<<|>>|\*\*|!=|::|\||&|\^|=|-|\+|,|.|<|>/)
-				@str = r
-				@t = OP
-				@v = case r
-					when ('<<' or '>>') then STREAM
-					when '**' then POWER
-					when '::' then SCOPE
-					when '=' then EQ
-					when '!=' then NEQ
-					when '+' then ADD
-					when '-' then SUB
-					when '::' then SCOPE
-					when '|' then OR
-					when ',' then COMMA
-					when ('<' or '>') then LT
-				end
-			elsif r = s.scan(/"[^"]*"/)
-				@t = STRING
-				@str = r
-			end
 		end
 
 		def inspect
 			case @t
-				when NUMBER then @v
 				when LABEL then @v
 				when OP then @str
 				when BRACKET then @str
 				when UNARY then @str
-				when STRING then @str
 				when COMMENT then @str
 			end
 		end
 	end
-	
-	class Lexer < StringScanner
-		attr_accessor :a
-		
+
+	class Fixnum < Object
+		def initialize n
+			super *[]
+			@v = n
+		end
+
+		def inspect; @v end
+	end
+
+	class PiString < Object
 		def initialize s
-			@a = Object.new
+			super *[]
+			@str = s
+		end
+
+		def inspect; @str end
+	end
+
+	class Lexer < StringScanner
+		attr_accessor :tokens
+		
+		def initialize s=''
+			@tokens = []
 			super s
 			
 			while !empty?
-				@a.arg << Object.new(self)
+				@tokens << if check(/#!(.*)\n/) && pos == 0
+					o = Object.new
+					o.t = Object::COMMENT
+					o.str = scan(/#!(.*)\n/)
+				elsif r = scan(/##\{([\s\S]*)\}|##(.*)\n/)
+					o = Object.new
+					o.t = Object::COMMENT
+					o.str = r
+				elsif r = scan(/-?[0-9]+([0-9a-zA-Z_$xXoOhHrRbB]*)/)
+					v = ''
+					base = 10
+					r.each_char {|c|
+						if base == 10
+							if 'hHxX$'[c]
+								base = 16
+							elsif 'oO'[c]
+								base = 8
+							elsif 'rR'[c]
+								base = v.to_i
+								v = ''
+							else
+								v += c
+							end
+						else
+							v += c
+						end
+					}
+				
+					if base == 10 and (v['b'] or v['B'])
+						r = v
+						v = ''
+						r.each_char {|c|
+							if '01'[c]
+								v += c
+							elsif 'bB'[c]
+								base = 2
+							end
+						}
+					end
+					
+					Pi::Fixnum.new v.to_i(base)
+				elsif r = scan(/[a-zA-Z$_][a-zA-Z0-9$_]*/)
+					o = Object.new
+					o.t = Object::LABEL
+					o.v = r
+				elsif r = scan(/[\(\)\[\]\{\}]/)
+					o = Object.new
+					o.str = r
+					o.t = Object::BRACKET
+					o.v = case r
+						when '['..']' then	Object::SQUARE
+						when '('..')' then	Object::ROUND
+						when '{'..'}' then	Object::CURLY
+					end
+					if ['[', '(', '{', '<'].include? r
+						o.v |= Object::LEFT
+					end
+				elsif r = scan(/\+\+|--|@|->|!/)
+					o = Object.new
+					o.str = r
+					o.t = Object::UNARY
+					o.v = case r
+						when '++' then Object::INC
+						when '--' then Object::DEC
+						when '@' then Object::AT
+						when '->' then Object::POINTER
+					end
+				elsif r = scan(/<<|>>|\*\*|!=|::|\||&|\^|=|-|\+|,|.|<|>/)
+					o = Object.new
+					o.str = r
+					o.t = Object::OP
+					o.v = case r
+						when ('<<' or '>>') then Object::STREAM
+						when '**' then Object::POWER
+						when '::' then Object::SCOPE
+						when '=' then Object::EQ
+						when '!=' then Object::NEQ
+						when '+' then Object::ADD
+						when '-' then Object::SUB
+						when '::' then Object::SCOPE
+						when '|' then Object::OR
+						when ',' then Object::COMMA
+						when ('<' or '>') then Object::LT
+					end
+				elsif r = scan(/"[^"]*"/)
+					PiString.new r
+				end
+
 				skip(/\s*/)
 			end
 		end
-		
+	end
+
+	class Parser
+		def initialize tree
+			@tree = tree
+		end
+
 		def parse
-			tree = []
-			
-			while not @a.arg.empty?
-				tree << parse_re
+			t = []
+
+			while not @tree.empty?
+				t << parse_re
 			end
 			
-			@a.arg = tree
+			t
 		end
 		
 		def parse_re
-			p @a.arg.length
-			t = @a.arg.slice! 0
+			p @tree.length
+			t = @tree.slice! 0
 			return nil if not t
 			
 			if t.t == Object::LABEL
-				arg = parse_re	#x(arg)
+				arg = parse_re
 				
 				if not arg
 					t.arg = []
@@ -194,7 +215,13 @@ module Pi
 			
 			return t
 		end
-		
+	end
+
+	class Compiler
+		def initialize
+
+		end
+
 		def compile
 			@a.arg.map {|e|
 				if e.t == Object::LABEL
@@ -205,22 +232,32 @@ module Pi
 				end
 			}.join
 		end
-		
-		def to_html
-			return '' if @a.arg.empty?
-			"<ul>" + @a.arg.map {|t|
-				"\t<li>#{t.to_html}</li>\n"
-			}.join + "</ul>"
-		end
 	end
 
-	def Pi.eval s #String/IO
+	def Pi.eval s
 		if s.is_a?(IO)
 			s = s.read
 		elsif !s.is_a?(String)
 			raise if !(s = String.try_convert s)
 		end
 
-		3 #...
+		parse(tokenize s)
+		
+	end
+
+	def Pi.tokenize s
+		if s.is_a?(IO)
+			s = s.read
+		elsif !s.is_a?(String)
+			raise if !(s = String.try_convert s)
+		end
+
+		l = Lexer.new s
+		l.tokens
+	end
+
+	def Pi.parse t
+		p = Parser.new t
+		p.parse
 	end
 end
